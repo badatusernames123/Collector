b6c16996ce5581c9faae4584a633ca9cedbc416d
badatusernames123 <52513266+badatusernames123@users.noreply.github.com>
2023-12-25 15:01:52 -0600

    Initial commit

---
+++ src/main/java/com/badatusernames/collector/CollectorClient.java
@@ -0,0 +1,10 @@
+package com.badatusernames.collector;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CollectorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
